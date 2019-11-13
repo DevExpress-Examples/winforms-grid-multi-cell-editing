8c27d5760185f491a882505d62b16d7d893d1091
DevExpressExampleBot <fakeEmail>
2019-11-13 21:18:00 +0300

    Source auto update [skip ci]

--- VB/DataHelper.vb
+++ VB/DataHelper.vb
@@ -5,17 +5,17 @@ Imports System.Linq
 Imports System.Text
 
 Namespace WindowsApplication1
-    Public Class DataHelper
-        Public Shared Function CreateTable(ByVal RowCount As Integer) As DataTable
-            Dim tbl As New DataTable()
-            tbl.Columns.Add("Name", GetType(String))
-            tbl.Columns.Add("ID", GetType(String))
-            tbl.Columns.Add("Number", GetType(String))
-            tbl.Columns.Add("Test", GetType(String))
-            For i As Integer = 0 To RowCount - 1
-                tbl.Rows.Add(New Object() { String.Format("Name{0}", i), i, 3 - i, "Test" })
-            Next i
-            Return tbl
-        End Function
-    End Class
+	Public Class DataHelper
+		Public Shared Function CreateTable(ByVal RowCount As Integer) As DataTable
+			Dim tbl As New DataTable()
+			tbl.Columns.Add("Name", GetType(String))
+			tbl.Columns.Add("ID", GetType(String))
+			tbl.Columns.Add("Number", GetType(String))
+			tbl.Columns.Add("Test", GetType(String))
+			For i As Integer = 0 To RowCount - 1
+				tbl.Rows.Add(New Object() { String.Format("Name{0}", i), i, 3 - i, "Test" })
+			Next i
+			Return tbl
+		End Function
+	End Class
 End Namespace
