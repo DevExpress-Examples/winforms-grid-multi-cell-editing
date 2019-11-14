2f52325cf4f68fcb1087989010adbde80929f9d0
CORP\tochilkin.alexander <13348208+AlexMedusa@users.noreply.github.com>
2019-11-14 19:55:00 +0300

    Revert "Source auto update [skip ci]"
    This reverts commit 0aeb26ccd3fe1612fad076f411630e42f5058406.

--- VB/Form1.Designer.vb
+++ VB/Form1.Designer.vb
@@ -1,117 +1,117 @@
 ﻿Namespace WindowsApplication1
-    Partial Public Class Form1
-        ''' <summary>
-        ''' Required designer variable.
-        ''' </summary>
-        Private components As System.ComponentModel.IContainer = Nothing
+	Partial Public Class Form1
+		''' <summary>
+		''' Required designer variable.
+		''' </summary>
+		Private components As System.ComponentModel.IContainer = Nothing
 
-        ''' <summary>
-        ''' Clean up any resources being used.
-        ''' </summary>
-        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-            If disposing AndAlso (components IsNot Nothing) Then
-                components.Dispose()
-            End If
-            MyBase.Dispose(disposing)
-        End Sub
+		''' <summary>
+		''' Clean up any resources being used.
+		''' </summary>
+		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+			If disposing AndAlso (components IsNot Nothing) Then
+				components.Dispose()
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
 
-        #Region "Windows Form Designer generated code"
+		#Region "Windows Form Designer generated code"
 
-        ''' <summary>
-        ''' Required method for Designer support - do not modify
-        ''' the contents of this method with the code editor.
-        ''' </summary>
-        Private Sub InitializeComponent()
-            Me.gridControl1 = New DevExpress.XtraGrid.GridControl()
-            Me.gridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
-            Me.gridColumn1 = New DevExpress.XtraGrid.Columns.GridColumn()
-            Me.gridColumn2 = New DevExpress.XtraGrid.Columns.GridColumn()
-            Me.gridColumn3 = New DevExpress.XtraGrid.Columns.GridColumn()
-            Me.gridColumn4 = New DevExpress.XtraGrid.Columns.GridColumn()
-            Me.gridView2 = New DevExpress.XtraGrid.Views.Grid.GridView()
-            CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-            CType(Me.gridView1, System.ComponentModel.ISupportInitialize).BeginInit()
-            CType(Me.gridView2, System.ComponentModel.ISupportInitialize).BeginInit()
-            Me.SuspendLayout()
-            ' 
-            ' gridControl1
-            ' 
-            Me.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill
-            Me.gridControl1.Location = New System.Drawing.Point(0, 0)
-            Me.gridControl1.MainView = Me.gridView1
-            Me.gridControl1.Name = "gridControl1"
-            Me.gridControl1.Size = New System.Drawing.Size(643, 506)
-            Me.gridControl1.TabIndex = 0
-            Me.gridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() { Me.gridView1, Me.gridView2})
-            ' 
-            ' gridView1
-            ' 
-            Me.gridView1.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() { Me.gridColumn1, Me.gridColumn2, Me.gridColumn3, Me.gridColumn4})
-            Me.gridView1.GridControl = Me.gridControl1
-            Me.gridView1.Name = "gridView1"
-            ' 
-            ' gridColumn1
-            ' 
-            Me.gridColumn1.Caption = "Name"
-            Me.gridColumn1.FieldName = "Name"
-            Me.gridColumn1.Name = "gridColumn1"
-            Me.gridColumn1.Visible = True
-            Me.gridColumn1.VisibleIndex = 0
-            ' 
-            ' gridColumn2
-            ' 
-            Me.gridColumn2.Caption = "ID"
-            Me.gridColumn2.FieldName = "ID"
-            Me.gridColumn2.Name = "gridColumn2"
-            Me.gridColumn2.Visible = True
-            Me.gridColumn2.VisibleIndex = 1
-            ' 
-            ' gridColumn3
-            ' 
-            Me.gridColumn3.Caption = "Number"
-            Me.gridColumn3.FieldName = "Number"
-            Me.gridColumn3.Name = "gridColumn3"
-            Me.gridColumn3.Visible = True
-            Me.gridColumn3.VisibleIndex = 2
-            ' 
-            ' gridColumn4
-            ' 
-            Me.gridColumn4.Caption = "Test"
-            Me.gridColumn4.FieldName = "Test"
-            Me.gridColumn4.Name = "gridColumn4"
-            Me.gridColumn4.Visible = True
-            Me.gridColumn4.VisibleIndex = 3
-            ' 
-            ' gridView2
-            ' 
-            Me.gridView2.GridControl = Me.gridControl1
-            Me.gridView2.Name = "gridView2"
-            ' 
-            ' Form1
-            ' 
-            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
-            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-            Me.ClientSize = New System.Drawing.Size(643, 506)
-            Me.Controls.Add(Me.gridControl1)
-            Me.Name = "Form1"
-            Me.Text = "Form1"
-            CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).EndInit()
-            CType(Me.gridView1, System.ComponentModel.ISupportInitialize).EndInit()
-            CType(Me.gridView2, System.ComponentModel.ISupportInitialize).EndInit()
-            Me.ResumeLayout(False)
+		''' <summary>
+		''' Required method for Designer support - do not modify
+		''' the contents of this method with the code editor.
+		''' </summary>
+		Private Sub InitializeComponent()
+			Me.gridControl1 = New DevExpress.XtraGrid.GridControl()
+			Me.gridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
+			Me.gridColumn1 = New DevExpress.XtraGrid.Columns.GridColumn()
+			Me.gridColumn2 = New DevExpress.XtraGrid.Columns.GridColumn()
+			Me.gridColumn3 = New DevExpress.XtraGrid.Columns.GridColumn()
+			Me.gridColumn4 = New DevExpress.XtraGrid.Columns.GridColumn()
+			Me.gridView2 = New DevExpress.XtraGrid.Views.Grid.GridView()
+			CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me.gridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me.gridView2, System.ComponentModel.ISupportInitialize).BeginInit()
+			Me.SuspendLayout()
+			' 
+			' gridControl1
+			' 
+			Me.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill
+			Me.gridControl1.Location = New System.Drawing.Point(0, 0)
+			Me.gridControl1.MainView = Me.gridView1
+			Me.gridControl1.Name = "gridControl1"
+			Me.gridControl1.Size = New System.Drawing.Size(643, 506)
+			Me.gridControl1.TabIndex = 0
+			Me.gridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() { Me.gridView1, Me.gridView2})
+			' 
+			' gridView1
+			' 
+			Me.gridView1.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() { Me.gridColumn1, Me.gridColumn2, Me.gridColumn3, Me.gridColumn4})
+			Me.gridView1.GridControl = Me.gridControl1
+			Me.gridView1.Name = "gridView1"
+			' 
+			' gridColumn1
+			' 
+			Me.gridColumn1.Caption = "Name"
+			Me.gridColumn1.FieldName = "Name"
+			Me.gridColumn1.Name = "gridColumn1"
+			Me.gridColumn1.Visible = True
+			Me.gridColumn1.VisibleIndex = 0
+			' 
+			' gridColumn2
+			' 
+			Me.gridColumn2.Caption = "ID"
+			Me.gridColumn2.FieldName = "ID"
+			Me.gridColumn2.Name = "gridColumn2"
+			Me.gridColumn2.Visible = True
+			Me.gridColumn2.VisibleIndex = 1
+			' 
+			' gridColumn3
+			' 
+			Me.gridColumn3.Caption = "Number"
+			Me.gridColumn3.FieldName = "Number"
+			Me.gridColumn3.Name = "gridColumn3"
+			Me.gridColumn3.Visible = True
+			Me.gridColumn3.VisibleIndex = 2
+			' 
+			' gridColumn4
+			' 
+			Me.gridColumn4.Caption = "Test"
+			Me.gridColumn4.FieldName = "Test"
+			Me.gridColumn4.Name = "gridColumn4"
+			Me.gridColumn4.Visible = True
+			Me.gridColumn4.VisibleIndex = 3
+			' 
+			' gridView2
+			' 
+			Me.gridView2.GridControl = Me.gridControl1
+			Me.gridView2.Name = "gridView2"
+			' 
+			' Form1
+			' 
+			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+			Me.ClientSize = New System.Drawing.Size(643, 506)
+			Me.Controls.Add(Me.gridControl1)
+			Me.Name = "Form1"
+			Me.Text = "Form1"
+			CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.gridView1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.gridView2, System.ComponentModel.ISupportInitialize).EndInit()
+			Me.ResumeLayout(False)
 
-        End Sub
+		End Sub
 
-        #End Region
+		#End Region
 
-        Private gridControl1 As DevExpress.XtraGrid.GridControl
-        Private gridView1 As DevExpress.XtraGrid.Views.Grid.GridView
-        Private gridView2 As DevExpress.XtraGrid.Views.Grid.GridView
-        Private gridColumn1 As DevExpress.XtraGrid.Columns.GridColumn
-        Private gridColumn2 As DevExpress.XtraGrid.Columns.GridColumn
-        Private gridColumn3 As DevExpress.XtraGrid.Columns.GridColumn
-        Private gridColumn4 As DevExpress.XtraGrid.Columns.GridColumn
-    End Class
+		Private gridControl1 As DevExpress.XtraGrid.GridControl
+		Private gridView1 As DevExpress.XtraGrid.Views.Grid.GridView
+		Private gridView2 As DevExpress.XtraGrid.Views.Grid.GridView
+		Private gridColumn1 As DevExpress.XtraGrid.Columns.GridColumn
+		Private gridColumn2 As DevExpress.XtraGrid.Columns.GridColumn
+		Private gridColumn3 As DevExpress.XtraGrid.Columns.GridColumn
+		Private gridColumn4 As DevExpress.XtraGrid.Columns.GridColumn
+	End Class
 End Namespace
 
--- VB/Form1.vb
+++ VB/Form1.vb
@@ -1,33 +1,34 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.ComponentModel
-Imports System.Data
-Imports System.Drawing
-Imports System.Text
-Imports System.Windows.Forms
+﻿Imports DevExpress.XtraEditors
 Imports DevExpress.XtraEditors.Repository
 Imports DevExpress.XtraGrid.Views.Grid
-Imports DevExpress.Utils
-Imports DevExpress.XtraGrid.Views.Base
-Imports DevExpress.XtraGrid.Views.Grid.ViewInfo
+Imports System
+Imports System.Windows.Forms
 
 Namespace WindowsApplication1
-    Partial Public Class Form1
-        Inherits Form
+	Partial Public Class Form1
+		Inherits Form
 
-        Public Sub New()
-            InitializeComponent()
-            gridControl1.DataSource = DataHelper.CreateTable(20)
-            gridView1.OptionsSelection.MultiSelect = True
-            gridView1.OptionsSelection.MultiSelectMode = GridMultiSelectMode.CellSelect
-            Dim ri As New RepositoryItemComboBox()
-            gridView1.Columns("Number").ColumnEdit = ri
+		Public Sub New()
+			InitializeComponent()
+			Dim radioGroup As New RadioGroup() With {.Dock = DockStyle.Top}
+			radioGroup.Height = 30
+			Controls.Add(radioGroup)
+			radioGroup.Properties.Columns = 3
+			radioGroup.Properties.Items.Add(New DevExpress.XtraEditors.Controls.RadioGroupItem(ChangeMode.All, "All"))
+			radioGroup.Properties.Items.Add(New DevExpress.XtraEditors.Controls.RadioGroupItem(ChangeMode.Row, "Row"))
+			radioGroup.Properties.Items.Add(New DevExpress.XtraEditors.Controls.RadioGroupItem(ChangeMode.Column, "Column"))
+			radioGroup.EditValue = ChangeMode.All
+			gridControl1.DataSource = DataHelper.CreateTable(20)
+			gridView1.OptionsSelection.MultiSelect = True
+			gridView1.OptionsSelection.MultiSelectMode = GridMultiSelectMode.CellSelect
+			Dim ri As New RepositoryItemComboBox()
+			gridView1.Columns("Number").ColumnEdit = ri
 
-            For i As Integer = 0 To 9
-                ri.Items.Add(String.Format("Test{0}", i))
-            Next i
+			For i As Integer = 0 To 9
+				ri.Items.Add(String.Format("Test{0}", i))
+			Next i
 
-            Dim tempVar As New MultiSelectionEditingHelper(gridView1)
-        End Sub
-    End Class
+			Dim tempVar As New MultiSelectionEditingHelper(gridView1, radioGroup)
+		End Sub
+	End Class
 End Namespace
