3b8cfaea4fde2644a711f47bf11e97fe029ea6ae
DevExpressExampleBot <fakeEmail>
2023-08-30 14:51:47 +0400

    Source auto update [skip ci]

--- VB/Form1.vb
+++ VB/Form1.vb
@@ -1,40 +1,41 @@
-﻿Imports DevExpress.XtraEditors
+Imports DevExpress.XtraEditors
 Imports DevExpress.XtraEditors.Repository
 Imports DevExpress.XtraGrid.Views.Grid
-Imports System
 Imports System.Windows.Forms
 
 Namespace WindowsApplication1
-	Partial Public Class Form1
-		Inherits Form
 
-		Private helper As MultiSelectionEditingHelper
-		Public Sub New()
-			InitializeComponent()
-			Dim radioGroup As New RadioGroup() With {.Dock = DockStyle.Top}
-			radioGroup.Height = 30
-			Controls.Add(radioGroup)
-			radioGroup.Properties.Columns = 3
-			radioGroup.Properties.Items.Add(New DevExpress.XtraEditors.Controls.RadioGroupItem(ChangeMode.All, "All"))
-			radioGroup.Properties.Items.Add(New DevExpress.XtraEditors.Controls.RadioGroupItem(ChangeMode.Row, "Row"))
-			radioGroup.Properties.Items.Add(New DevExpress.XtraEditors.Controls.RadioGroupItem(ChangeMode.Column, "Column"))
-			radioGroup.EditValue = ChangeMode.All
-			gridControl1.DataSource = DataHelper.CreateTable(20)
-			gridView1.OptionsSelection.MultiSelect = True
-			gridView1.OptionsSelection.MultiSelectMode = GridMultiSelectMode.CellSelect
-			Dim ri As New RepositoryItemComboBox()
-			gridView1.Columns("Number").ColumnEdit = ri
+    Public Partial Class Form1
+        Inherits Form
 
-			For i As Integer = 0 To 9
-				ri.Items.Add(String.Format("Test{0}", i))
-			Next i
+        Private helper As MultiSelectionEditingHelper
 
-			helper = New MultiSelectionEditingHelper(gridView1, radioGroup)
-		End Sub
-		Protected Overrides Sub OnFormClosing(ByVal e As FormClosingEventArgs)
-			helper.Disable()
-			helper = Nothing
-			MyBase.OnFormClosing(e)
-		End Sub
-	End Class
+        Public Sub New()
+            InitializeComponent()
+            Dim radioGroup As RadioGroup = New RadioGroup() With {.Dock = DockStyle.Top}
+            radioGroup.Height = 30
+            Controls.Add(radioGroup)
+            radioGroup.Properties.Columns = 3
+            radioGroup.Properties.Items.Add(New DevExpress.XtraEditors.Controls.RadioGroupItem(ChangeMode.All, "All"))
+            radioGroup.Properties.Items.Add(New DevExpress.XtraEditors.Controls.RadioGroupItem(ChangeMode.Row, "Row"))
+            radioGroup.Properties.Items.Add(New DevExpress.XtraEditors.Controls.RadioGroupItem(ChangeMode.Column, "Column"))
+            radioGroup.EditValue = ChangeMode.All
+            gridControl1.DataSource = DataHelper.CreateTable(20)
+            gridView1.OptionsSelection.MultiSelect = True
+            gridView1.OptionsSelection.MultiSelectMode = GridMultiSelectMode.CellSelect
+            Dim ri As RepositoryItemComboBox = New RepositoryItemComboBox()
+            gridView1.Columns("Number").ColumnEdit = ri
+            For i As Integer = 0 To 10 - 1
+                ri.Items.Add(String.Format("Test{0}", i))
+            Next
+
+            helper = New MultiSelectionEditingHelper(gridView1, radioGroup)
+        End Sub
+
+        Protected Overrides Sub OnFormClosing(ByVal e As FormClosingEventArgs)
+            helper.Disable()
+            helper = Nothing
+            MyBase.OnFormClosing(e)
+        End Sub
+    End Class
 End Namespace
--- VB/MultiSelectionEditingHelper.vb
+++ VB/MultiSelectionEditingHelper.vb
@@ -1,5 +1,4 @@
-﻿Imports System
-Imports System.Collections.Generic
+Imports System
 Imports System.Windows.Forms
 Imports DevExpress.XtraGrid.Views.Grid
 Imports DevExpress.Utils
@@ -9,88 +8,93 @@ Imports DevExpress.XtraEditors
 Imports DevExpress.XtraGrid.Columns
 
 Namespace WindowsApplication1
-	Public Class MultiSelectionEditingHelper
 
-		Private view As GridView
-		Private radioGroup As RadioGroup
-		Public Sub New(ByVal view As GridView, ByVal radioGroup As RadioGroup)
-			Me.radioGroup = radioGroup
-			Me.view = view
-			Me.view.OptionsBehavior.EditorShowMode = EditorShowMode.MouseDownFocused
-			AddHandler view.MouseUp, AddressOf view_MouseUp
-			AddHandler view.CellValueChanged, AddressOf view_CellValueChanged
-			AddHandler view.MouseDown, AddressOf view_MouseDown
-		End Sub
+    Public Class MultiSelectionEditingHelper
 
-		Private Sub view_MouseDown(ByVal sender As Object, ByVal e As MouseEventArgs)
-			If GetInSelectedCell(e) Then
-				Dim hi As GridHitInfo = view.CalcHitInfo(e.Location)
-				If view.FocusedRowHandle = hi.RowHandle Then
-					view.FocusedColumn = hi.Column
-					DXMouseEventArgs.GetMouseArgs(e).Handled = True
-				End If
-			End If
-		End Sub
+        Private view As GridView
 
-		Private Sub view_CellValueChanged(ByVal sender As Object, ByVal e As CellValueChangedEventArgs)
-			OnCellValueChanged(e)
-		End Sub
+        Private radioGroup As RadioGroup
 
-		Private lockEvents As Boolean
-		Private Sub OnCellValueChanged(ByVal e As CellValueChangedEventArgs)
-			If lockEvents Then
-				Return
-			End If
-			lockEvents = True
-			SetSelectedCellsValues(e.Value)
-			lockEvents = False
-		End Sub
+        Public Sub New(ByVal view As GridView, ByVal radioGroup As RadioGroup)
+            Me.radioGroup = radioGroup
+            Me.view = view
+            Me.view.OptionsBehavior.EditorShowMode = EditorShowMode.MouseDownFocused
+            AddHandler Me.view.MouseUp, AddressOf view_MouseUp
+            AddHandler Me.view.CellValueChanged, AddressOf view_CellValueChanged
+            AddHandler Me.view.MouseDown, AddressOf view_MouseDown
+        End Sub
 
-		Private Sub SetSelectedCellsValues(ByVal value As Object)
-			Try
-				view.BeginUpdate()
-				Dim cells() As GridCell = view.GetSelectedCells()
-				Dim mode As ChangeMode = CType(radioGroup.EditValue, ChangeMode)
-				For Each cell As GridCell In cells
-					Dim rowHandle As Integer = cell.RowHandle
-					Dim column As GridColumn = cell.Column
-					Select Case mode
-						Case ChangeMode.All
-						Case ChangeMode.Column
-							column = view.FocusedColumn
-						Case ChangeMode.Row
-							rowHandle = view.FocusedRowHandle
-					End Select
-					view.SetRowCellValue(rowHandle, column, value)
-				Next cell
-			Catch ex As Exception
-			Finally
-				view.EndUpdate()
-			End Try
-		End Sub
+        Private Sub view_MouseDown(ByVal sender As Object, ByVal e As MouseEventArgs)
+            If GetInSelectedCell(e) Then
+                Dim hi As GridHitInfo = view.CalcHitInfo(e.Location)
+                If view.FocusedRowHandle = hi.RowHandle Then
+                    view.FocusedColumn = hi.Column
+                    DXMouseEventArgs.GetMouseArgs(e).Handled = True
+                End If
+            End If
+        End Sub
 
-		Private Function GetInSelectedCell(ByVal e As MouseEventArgs) As Boolean
-			Dim hi As GridHitInfo = view.CalcHitInfo(e.Location)
-			Return hi.InRowCell AndAlso view.IsCellSelected(hi.RowHandle, hi.Column)
-		End Function
+        Private Sub view_CellValueChanged(ByVal sender As Object, ByVal e As CellValueChangedEventArgs)
+            OnCellValueChanged(e)
+        End Sub
 
-		Private Sub view_MouseUp(ByVal sender As Object, ByVal e As MouseEventArgs)
-			Dim inSelectedCell As Boolean = GetInSelectedCell(e)
-			If inSelectedCell Then
-				DXMouseEventArgs.GetMouseArgs(e).Handled = True
-				view.ShowEditorByMouse()
-			End If
-		End Sub
-		Public Sub Disable()
-			RemoveHandler view.MouseUp, AddressOf view_MouseUp
-			RemoveHandler view.CellValueChanged, AddressOf view_CellValueChanged
-			RemoveHandler view.MouseDown, AddressOf view_MouseDown
-			view = Nothing
-		End Sub
-	End Class
-	Public Enum ChangeMode
-		All
-		Row
-		Column
-	End Enum
+        Private lockEvents As Boolean
+
+        Private Sub OnCellValueChanged(ByVal e As CellValueChangedEventArgs)
+            If lockEvents Then Return
+            lockEvents = True
+            SetSelectedCellsValues(e.Value)
+            lockEvents = False
+        End Sub
+
+        Private Sub SetSelectedCellsValues(ByVal value As Object)
+            Try
+                view.BeginUpdate()
+                Dim cells As GridCell() = view.GetSelectedCells()
+                Dim mode As ChangeMode = CType(radioGroup.EditValue, ChangeMode)
+                For Each cell As GridCell In cells
+                    Dim rowHandle As Integer = cell.RowHandle
+                    Dim column As GridColumn = cell.Column
+                    Select Case mode
+                        Case ChangeMode.All
+                        Case ChangeMode.Column
+                            column = view.FocusedColumn
+                        Case ChangeMode.Row
+                            rowHandle = view.FocusedRowHandle
+                    End Select
+
+                    view.SetRowCellValue(rowHandle, column, value)
+                Next
+            Catch ex As Exception
+            Finally
+                view.EndUpdate()
+            End Try
+        End Sub
+
+        Private Function GetInSelectedCell(ByVal e As MouseEventArgs) As Boolean
+            Dim hi As GridHitInfo = view.CalcHitInfo(e.Location)
+            Return hi.InRowCell AndAlso view.IsCellSelected(hi.RowHandle, hi.Column)
+        End Function
+
+        Private Sub view_MouseUp(ByVal sender As Object, ByVal e As MouseEventArgs)
+            Dim inSelectedCell As Boolean = GetInSelectedCell(e)
+            If inSelectedCell Then
+                DXMouseEventArgs.GetMouseArgs(e).Handled = True
+                view.ShowEditorByMouse()
+            End If
+        End Sub
+
+        Public Sub Disable()
+            RemoveHandler view.MouseUp, AddressOf view_MouseUp
+            RemoveHandler view.CellValueChanged, AddressOf view_CellValueChanged
+            RemoveHandler view.MouseDown, AddressOf view_MouseDown
+            view = Nothing
+        End Sub
+    End Class
+
+    Public Enum ChangeMode
+        All
+        Row
+        Column
+    End Enum
 End Namespace
